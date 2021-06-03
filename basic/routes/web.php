ba869b4ae993588d8309a817a65b34bd37395887
NelsonWang <wangnelson2@gmail.com>
2021-06-02 20:58:45 -0400

    Laravel 8 | basic | web

--- basic/routes/web.php
+++ basic/routes/web.php
@@ -3,14 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
+use App\Http\Middleware\CheckAge;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    echo  "This is Home Page";
+});
+
 Route::get('/about', function () {
     return view('about');
-});
+})->middleware('check');
 
 // Route::get('/contact', 'ContactController@index');
 Route::get('/contact', [ContactController::class, 'index']);
